e812c4ab48e0d414f336f5537f68a8878c673575
David Harting <david@emplify.com>
2017-03-13 10:02:01 -0400

    Theme the top bar of the menu It is more of a heading and it is used to close the drawer, so it should not look like a menu item. Made it look more like the app bar.

--- src/modules/drawer/index.js
+++ src/modules/drawer/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import MuiDrawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
+import muiThemeable from 'material-ui/styles/muiThemeable';
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as drawerActions from './dux'
 
 class Drawer extends React.Component {
   render() {
+    const theme = this.props.muiTheme
+    const topBarStyle = {
+     backgroundColor: theme.palette.primary1Color,
+     color: theme.appBar.textColor
+     
+    }
     return (
       <div>
         <MuiDrawer open={this.props.drawer.isOpen}>
+          <MenuItem
+            primaryText='Apps'
+            onTouchTap={this.props.actions.closeDrawer}
+            innerDivStyle={topBarStyle}
+          />
           <MenuItem primaryText='Counter' />
           <MenuItem primaryText='Todo List' />
         </MuiDrawer>
@@ -22,4 +36,10 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(Drawer)
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(drawerActions, dispatch)
+  }
+}
+
+export default muiThemeable()(connect(mapStateToProps, mapDispatchToProps)(Drawer))
